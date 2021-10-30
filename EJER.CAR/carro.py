b1c94bd31f9f4e0d6dba5c7c737f3a610c371f30
Hellen02Henandez2021 <87906169+Hellen02Henandez2021@users.noreply.github.com>
2021-10-30 10:49:15 -0600

    PRIMER COMMIT

---
+++ EJER.CAR/carro.py
@@ -0,0 +1,7 @@
+class carro:
+    def encender(self):
+        return"carro encendido"
+    def Acelerar(self):
+        return"carro en marcha"
+    def frenar(self):
+        return"frenando carro"
